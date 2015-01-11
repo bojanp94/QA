6624cd062c037c6a543ad37234ed2d0eecae121f
bojanp94 <petrovskib94@gmail.com>
2015-01-11 03:52:55 +0100

    Revert "ForgotPasswordPage"
    This reverts commit a06bc7f96e4652af8fb07a35d71ef03fe9f8f344.

--- ForgotPasswordPage.java
+++ ForgotPasswordPage.java
@@ -1,6 +1,4 @@
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.testng.Assert;
 
 
 public class ForgotPasswordPage {
@@ -13,21 +11,6 @@ public class ForgotPasswordPage {
 			this.driver = driver;
 		else
 			throw new IllegalStateException("This is not the login page:" + driver.getCurrentUrl());
-	}
-	public LoginPage sendRequest(String email)
-	{
-		driver.findElement(By.id("txtEmail_I")).clear();
-		driver.findElement(By.id("txtEmail_I")).sendKeys(email);
-		driver.findElement(By.id("btnSend_CD")).click();
-		driver.findElement(By.id("btnOK_CD")).click();
-		
-		return new LoginPage(driver);
-	}
-	public LoginPage cancel()
-	{
-		driver.findElement(By.id("btnCancel_CD")).click();
-		
-		return new LoginPage(driver);
 	}
 
 }
